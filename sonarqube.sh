370b882495cf1574adb35a19c99165a9c2940d58
naginenihari <naginenihari@gmail.com>
2026-01-08 19:15:22 +0530

    SonarQube Docker file is created

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
-#resize disk from 20GB to 50GB
-## growpart /dev/xvda 4
+############################
+# Disk resize
+############################
 sudo growpart /dev/nvme0n1 4
 
 sudo lvextend -L +10G /dev/mapper/RootVG-varVol
@@ -12,17 +14,51 @@ sudo xfs_growfs /
 sudo xfs_growfs /var
 sudo xfs_growfs /home
 
-# docker
+############################
+# Kernel requirements
+############################
+sudo sysctl -w vm.max_map_count=524288
+echo "vm.max_map_count=524288" | sudo tee -a /etc/sysctl.conf
+
+############################
+# Docker installation
+############################
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 systemctl enable docker
+systemctl start docker
 usermod -aG docker ec2-user
-systemctl restart docker
-sudo sysctl -w vm.max_map_count=262144
-docker pull sonarqube
-docker run -d --name sonarqube-db -e POSTGRES_USER=sonar -e POSTGRES_PASSWORD=sonar -e POSTGRES_DB=sonarqube postgres:alpine
-docker run -d --name sonarqube -p 9000:9000 --link sonarqube-db:db -e SONAR_JDBC_URL=jdbc:postgresql://db:5432/sonarqube -e SONAR_JDBC_USERNAME=sonar -e SONAR_JDBC_PASSWORD=sonar sonarqube
 
+############################
+# Docker network
+############################
+docker network create sonarnet || true
 
+############################
+# PostgreSQL (SUPPORTED)
+############################
+docker run -d \
+  --name sonarqube-db \
+  --network sonarnet \
+  -e POSTGRES_USER=sonar \
+  -e POSTGRES_PASSWORD=sonar \
+  -e POSTGRES_DB=sonarqube \
+  -v sonardb:/var/lib/postgresql/data \
+  postgres:13-alpine
+
+############################
+# SonarQube (LTS ONLY)
+############################
+docker run -d \
+  --name sonarqube \
+  --network sonarnet \
+  -p 9000:9000 \
+  -e SONAR_JDBC_URL=jdbc:postgresql://sonarqube-db:5432/sonarqube \
+  -e SONAR_JDBC_USERNAME=sonar \
+  -e SONAR_JDBC_PASSWORD=sonar \
+  -v sonarqube_data:/opt/sonarqube/data \
+  -v sonarqube_extensions:/opt/sonarqube/extensions \
+  -v sonarqube_logs:/opt/sonarqube/logs \
+  sonarqube:9.9-community
